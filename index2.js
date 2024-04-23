16c426f37227eb755bb83c98e1b01af0d83add7f
SaraGarciaM <saragarciamatos@gmail.com>
2024-04-23 21:59:04 +0100

    File renames

--- index2.js
+++
@@ -1,5 +0,0 @@
-const test = document.getElementById("name-input");
-console.log(document.location.search);
-const params = new URLSearchParams(document.location.search);
-let firstName = params.get("firstName");
-console.log(firstName);
